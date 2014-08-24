acd4ee80819f58ee2b27dd1d2c10bc2d30b13b9e
Tõnis Simo <anton.estum@gmail.com>
2014-08-24 20:30:54 +0400

    Fix dependencies

--- lib/sidekiq/perform_once.rb
+++ lib/sidekiq/perform_once.rb
@@ -1,4 +1,6 @@
 require "active_support/concern"
+require "sidekiq/perform_once/base"
+require "sidekiq/perform_once/delayed"
 
 module Sidekiq
   module PerformOnce
@@ -6,11 +8,11 @@ module Sidekiq
     
     module ClassMethods
       def perform_once(*args)
-        Base.new(self, *args).keep_one_job_or_perform!
+        Sidekiq::PerformOnce::Base.new(self, *args).keep_one_job_or_perform!
       end
             
       def perform_once_in(delay, *args)
-        Delayed.new(self, delay.to_i, *args).keep_one_job_or_perform!
+        Sidekiq::PerformOnce::Delayed.new(self, delay.to_i, *args).keep_one_job_or_perform!
       end
       
       alias_method :perform_once_now, :perform_once
--- lib/sidekiq_perform_once.rb
+++ lib/sidekiq_perform_once.rb
@@ -1,4 +1,2 @@
 require "sidekiq/api"
 require "sidekiq/perform_once"
-require "sidekiq/perform_once/base"
-require "sidekiq/perform_once/delayed"
